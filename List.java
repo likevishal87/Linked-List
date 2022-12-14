bbaeeed50f16b3d3d0f4e9e2f0012784d0d76db6
Vicky Verma <likevishalverma@gmail.com>
2022-12-15 03:16:53 +0530

    Add UC6 - delete the last element in the LinkedList

---
+++ List.java
@@ -0,0 +1,75 @@
+package com.bridgelabz;
+
+public class List {
+	Node head;
+	Node tail;
+	
+	public static void main(String[] args) {
+		System.out.println("Let's create a simple Linked List of 56, 30, 70");
+		
+		List nodeMethods = new List();
+		nodeMethods.addNode(56);
+		nodeMethods.addNode(30);
+		nodeMethods.addNode(70);
+		nodeMethods.printLinkedList();	
+		
+		System.out.println("\nAfter deleting the last element");
+		nodeMethods.pop(70);
+		nodeMethods.printLinkedList();
+	}
+	
+	public void addNode(int element) {
+		Node newNode = new Node(element);
+		
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		}
+		else {
+			tail.next = newNode;
+			tail = newNode;
+		}
+	}
+	
+	public void insert(int element) {
+		Node newNode = new Node(element);
+		
+		Node thisNode = head;
+		while (thisNode.data != 56)
+			thisNode = thisNode.next;
+		
+		newNode.next = thisNode.next;
+		thisNode.next = newNode;
+	}
+	
+	public void pop(int dataToRemove) {
+		Node thisNode = head;
+		
+		if (dataToRemove == head.data) {
+			thisNode = head.next;
+			head = thisNode;
+		}
+		else {
+			Node previous = null;
+			while (thisNode.data != dataToRemove) {
+				previous = thisNode;
+				thisNode = thisNode.next;
+			}
+			previous.next = thisNode.next;	
+		}
+	}
+	
+	public void printLinkedList() {
+		Node thisNode = head;
+		
+		if (thisNode == null) 
+			System.out.println("\nThe Linked list is empty.\n");
+		else {
+			System.out.println("\nDisplaying the Linked List : \n");
+			while (thisNode != null) {
+				System.out.print(thisNode.data + "\s");
+				thisNode = thisNode.next;
+			} 
+		}
+	}
+}
